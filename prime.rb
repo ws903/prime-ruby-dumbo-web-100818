d049b7f826624c5fa2b57c48599b404026eb0af5
David Sim <wsim903@gmail.com>
2018-09-26 19:05:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num > 1
+    for n in 2...num do
+      if num % n == 0
+        return false
+      end
+    end
+    true
+  else
+    false
+  end
+end
+
+puts prime?(4)
